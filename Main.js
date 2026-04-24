d1c1b7f3166471726bf27f107214721ae688e24e
Esguerra-9-R-ay2526 <esguerra.lln@obmontessori.edu.ph>
2026-04-24 12:34:05 +0800

    update main

---
+++ Main.js
@@ -0,0 +1,7 @@
+function countAnimals(){
+    var animal = ["Girrafe", "Cheetah", "Lion", "Elephant"]
+
+    for (var i = 0; i < animal.length; i++){
+        window.alert("This zoo contains a ")
+    }
+}
